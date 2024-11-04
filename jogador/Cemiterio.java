7ec2a5b5352c68e1ab3fea42dda6bbf2ceb909b4
EVSSD <114709962+EVSSD@users.noreply.github.com>
2024-11-04 19:03:33 -0300

    Add files via upload
    Inicial de jogador

---
+++ jogador/Cemiterio.java
@@ -0,0 +1,10 @@
+package jogador;
+import cartas.Cartas;
+import cartas.ColecaoDeCartas;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cemiterio {
+	private List<ColecaoDeCartas> cartas;
+
+}
---
+++ jogador/Jogador.java
@@ -0,0 +1,11 @@
+package jogador;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Jogador {
+	private int hp;
+	private int mana;
+	private Deck deck;
+	private Cemiterio cemiterio;
+
+}

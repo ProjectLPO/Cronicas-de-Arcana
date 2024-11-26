3e79457a0b684cac902b926af1eff777c43e9ef2
EVSSD <114709962+EVSSD@users.noreply.github.com>
2024-11-25 21:51:48 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+import menu.MenuPrincipal;
+
+
+public class Main {
+  public static void main(String[] args) {
+    	        // Inicializa o menu principal e inicia o programa
+    	        MenuPrincipal menu = new MenuPrincipal();
+    	        menu.iniciar();
+    	    }
+    	}
